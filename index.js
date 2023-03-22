ad69f60280a6ce57725504cb051b5b93d04df466
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-03-22 14:36:08 +0100

    Init

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Visleaf {
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+}
+
+const v1 = new Visleaf(0, 0, 10, 10);
+const v2 = new Visleaf(0, 0, 10, 15);
